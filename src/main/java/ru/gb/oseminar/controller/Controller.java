9d112e6bf35d2e0f40057aada59dcf3b66c9743d
Nord.A <mr.nordakov@gmail.com>
2022-12-07 14:36:44 +0300

    add OOP HW#2

--- src/main/java/ru/gb/oseminar/controller/Controller.java
+++ src/main/java/ru/gb/oseminar/controller/Controller.java
@@ -1,19 +1,43 @@
 package ru.gb.oseminar.controller;
 
 
+import ru.gb.oseminar.data.Student;
+import ru.gb.oseminar.data.StudyGroup;
+import ru.gb.oseminar.data.Teacher;
 import ru.gb.oseminar.data.User;
+import ru.gb.oseminar.sevice.StudyGroupService;
 import ru.gb.oseminar.sevice.UserService;
 import ru.gb.oseminar.view.StudentView;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Controller {
     public UserService userService = new UserService();
     public StudentView studentView = new StudentView();
+    public StudyGroupService studyGroupService = new StudyGroupService();
+
 
     public void createUser (String firstName, String lastName, String patronymic) {
         userService.createUser(firstName, lastName, patronymic);
         List<User> students = userService.getAll();
         studentView.sendOnConsole(students);
+    }
+
+    public void createStudyGroup (){
+        List<User> users = this.userService.getAll();
+        Teacher teacher = null;
+        List<Student> students = new ArrayList<>();
+
+        for (User user : users) {
+            if (user instanceof Teacher) {
+                teacher = (Teacher) user;
+            } else if (user instanceof Student) {
+                students.add((Student) user);
+            }
+
+        }
+        this.studyGroupService.createStudyGroup(teacher, students);
+        StudyGroup studyGroup = this.studyGroupService.getStudyGroup();
     }
 }
---
+++ src/main/java/ru/gb/oseminar/sevice/StudyGroupService.java
@@ -0,0 +1,28 @@
+package ru.gb.oseminar.sevice;
+
+
+import ru.gb.oseminar.data.Student;
+import ru.gb.oseminar.data.StudyGroup;
+import ru.gb.oseminar.data.Teacher;
+
+import java.util.List;
+
+public class StudyGroupService {
+    private StudyGroup studyGroup;
+
+    public StudyGroupService(){
+
+    }
+
+    public void createStudyGroup(Teacher teacher, List<Student> students) {
+        this.studyGroup = new StudyGroup(teacher, students);
+    }
+
+    public StudyGroup getStudyGroup() {
+        return studyGroup;
+    }
+
+    public void setStudyGroup(StudyGroup studyGroup) {
+        this.studyGroup = studyGroup;
+    }
+}
